df9ff0dd731768015d43944dd91089b3999cbc98
alanhuuu <147261547+alanhuuu@users.noreply.github.com>
2026-01-24 16:28:06 -0500

    feat: fix token issues

--- focusflow-backend/focusflow-backend/services/token_generator.py
+++ focusflow-backend/focusflow-backend/services/token_generator.py
@@ -1,10 +1,19 @@
 import jwt
 import time
 import os
+from pathlib import Path
 
-APPLE_TEAM_ID = os.getenv("APPLE_TEAM_ID")
-APPLE_KEY_ID = os.getenv("APPLE_KEY_ID")
-PRIVATE_KEY_PATH = os.getenv("APPLE_PRIVATE_KEY_PATH")
+# APPLE_TEAM_ID = os.getenv("APPLE_TEAM_ID")
+# APPLE_KEY_ID = os.getenv("APPLE_KEY_ID")
+# PRIVATE_KEY_PATH = os.getenv("APPLE_PRIVATE_KEY_PATH")
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+APPLE_TEAM_ID = "Q7ZLXMG4SB"
+APPLE_KEY_ID = "88B2L2KSK6"
+PRIVATE_KEY_PATH = "./keys/AuthKey_88B2L2KSK6.p8"
+
+
 
 def generate_developer_token():
     with open(PRIVATE_KEY_PATH, "r") as f:
